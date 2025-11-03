28ec5fa14c2d01cfaae99b21e21b7f37c509d9aa
mironovv <1111111111aleksey46@gmail.com>
2025-11-03 13:36:53 +0300

    add requirements and modify redis connect

--- rdis/redis_listener.py
+++ rdis/redis_listener.py
@@ -8,30 +8,32 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-async def start_redis_listener(bot : Bot ):
+async def start_redis_listener(bot):
     redis = aioredis.from_url(REDIS_URL)
-    logger.info("Redis listener запущен и ожидает сообщения в 'telegram:responses'...")
+    logger.info("Redis listener запущен и ожидает сообщения...")
 
-    while True:
-        try:
-            result = await redis.blpop("telegram:responses", timeout=0) 
-
+    try:
+        while True:
+            result = await redis.blpop("telegram:responses", timeout=0)
             if not result:
-                continue
-            
-            if result:
-                key, data = result  
+                continue  
 
-                if isinstance(data, bytes):
-                    data = data.decode('utf-8')
+            key, data = result
+            if isinstance(data, bytes):
+                data = data.decode('utf-8')
 
+            try:
                 resp = json.loads(data)
                 await bot.send_message(resp["user_id"], resp["text"])
                 logger.debug(f"Отправлено сообщение пользователю {resp['user_id']}")
+            except Exception as e:
+                logger.error(f"Не удалось обработать сообщение: {data}, ошибка: {e}")
 
-        except asyncio.CancelledError:
-            logger.info("Redis listener был остановлен.")
-            break
-        except Exception as e:
-            logger.error(f"Ошибка в Redis listener: {e}")
-            await asyncio.sleep(1)  
+    except asyncio.CancelledError:
+        logger.info("Redis listener остановлен по запросу.")
+        raise  
+    except Exception as e:
+        logger.critical(f"Критическая ошибка в listener: {e}")
+        raise
+    finally:
+        await redis.close()
